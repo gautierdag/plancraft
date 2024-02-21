42ee7430c98ea58ac2ef42332a0ac4fa023fba1e
Gautier Dagan <gautier.dagan@ed.ac.uk>
2024-02-21 12:07:20 +0000

    extract dep from evaluation

--- dep.py
+++ dep.py
@@ -4,6 +4,13 @@ import logging
 import time
 
 from openai import OpenAI
+import os
+import torch
+from models import CraftAgent, MineAgent
+from models.utils import slice_obs, stack_obs
+
+prefix = os.getcwd()
+goal_mapping_json = os.path.join(prefix, "data/goal_mapping.json")
 
 logger = logging.getLogger(__name__)
 
@@ -90,7 +97,7 @@ class Planner:
             context = f.read()
         return context
 
-    def load_initial_planning_prompt(self, group):
+    def load_initial_planning_prompt(self):
         with open(self.task_prompt_file, "r") as f:
             context = f.read()
         with open(self.replan_prompt_file, "r") as f:
@@ -239,12 +246,12 @@ class Planner:
         logger.info(f"Current Plan is {goal_list}")
         return goal_list
 
-    def initial_planning(self, group, task_question):
-        task_prompt = self.load_initial_planning_prompt(group)
+    def initial_planning(self, task_question):
+        task_prompt = self.load_initial_planning_prompt()
         question = f"Human: {task_question}\n"
         task_prompt_text = task_prompt + question
         plan = self.query_codex(task_prompt_text)
-        self.dialogue = self.load_initial_planning_prompt(group) + question
+        self.dialogue = self.load_initial_planning_prompt() + question
         self.dialogue += plan
         self.logging_dialogue = question
         self.logging_dialogue += plan
@@ -303,3 +310,212 @@ class Planner:
     def save_dialogue(self, filename: str):
         with open(filename, "w") as f:
             f.write(self.logging_dialogue)
+
+
+class DEP:
+    def __init__(self, cfg, env):
+        self.goal_mapping_cfg = self.load_goal_mapping_config()
+        self.mineclip_prompt_dict = self.goal_mapping_cfg["mineclip"]
+
+        # unify the mineclip and clip
+        self.clip_prompt_dict = self.goal_mapping_cfg["clip"]
+        self.goal_mapping_dict = self.goal_mapping_cfg["horizon"]
+
+        no_op = env.action_space.no_op()
+        self.miner = MineAgent(cfg, no_op=no_op)
+        self.crafter = CraftAgent(env, no_op=no_op)
+        self.planner = Planner()
+
+    def load_goal_mapping_config(self):
+        with open(goal_mapping_json, "r") as f:
+            goal_mapping_cfg = json.load(f)
+        return goal_mapping_cfg
+
+    def check_inventory(
+        self, inventory, items: dict
+    ):  # items: {"planks": 4, "stick": 2}
+        for key in items.keys():  # check every object item
+            if (
+                sum([item["quantity"] for item in inventory if item["name"] == key])
+                < items[key]
+            ):
+                return False
+        return True
+
+    def check_precondition(self, inventory, precondition: dict):
+        for key in precondition.keys():  # check every object item
+            if (
+                sum([item["quantity"] for item in inventory if item["name"] == key])
+                < precondition[key]
+            ):
+                return False
+        return True
+
+    def replan_task(self, inventory, task_question):
+        self.planner.generate_failure_description(self.curr_goal["ranking"])
+        self.planner.generate_inventory_description(inventory)
+        self.planner.generate_explanation()
+        plan = self.planner.replan(task_question)
+
+        self.goal_list = self.planner.generate_goal_list(plan)
+        if len(self.goal_list) == 0:
+            self.curr_goal = {
+                "name": "mine_log",
+                "type": "mine",
+                "object": {"log": 1},
+                "precondition": {},
+                "ranking": 1,
+            }
+        else:
+            self.curr_goal = self.goal_list[0]
+        self.goal_eps = 0
+        self.replan_rounds += 1
+
+    def update_goal(self, inventory):
+        if (
+            self.check_inventory(inventory, self.curr_goal["object"])
+            and self.goal_eps > 1
+        ):
+            logger.info(f"finish goal {self.curr_goal['name']}.")
+            self.planner.generate_success_description(self.curr_goal["ranking"])
+            self.goal_list.remove(self.goal_list[0])
+            self.curr_goal = self.goal_list[0]
+            self.goal_eps = 0
+
+    def reset(self, task_question: str):
+        self.task_question = task_question
+
+        self.planner.reset()
+        plan = self.planner.initial_planning(task_question)
+        self.goal_list = self.planner.generate_goal_list(plan)
+        # NOTE: if the goal list is empty, then the DEP default goal is to mine a log
+        if len(self.goal_list) == 0:
+            self.curr_goal = {
+                "name": "mine_log",
+                "type": "mine",
+                "object": {"log": 1},
+                "precondition": {},
+                "ranking": 1,
+            }
+        else:
+            self.curr_goal = self.goal_list[0]
+        self.goal_eps = 0
+        self.replan_rounds = 0
+        self.seek_point = 0
+        self.goal_history = []
+        self.prev_goal = None
+        self.actions = torch.zeros(1, self.miner.model.action_dim)
+        self.states = None
+
+    def step(self, obs, info, t=0):
+        if self.states is None:
+            self.states = obs
+        else:
+            self.states = stack_obs(self.states, obs)
+
+        self.update_goal(info["inventory"])
+        curr_goal = self.curr_goal
+
+        self.goal_history.append(curr_goal)
+
+        if not self.prev_goal == curr_goal:
+            logger.info(f"Episode Step {t}, Current Goal {curr_goal}")
+            self.seek_point = t
+            self.actions = torch.zeros(
+                self.actions.shape[0], self.miner.model.action_dim
+            )
+
+        self.prev_goal = curr_goal
+
+        curr_goal_type = self.curr_goal["type"]
+
+        # check if the inventory has the preconditions
+        if curr_goal_type == "mine" and not self.check_precondition(
+            info["inventory"], self.curr_goal["precondition"]
+        ):
+            self.replan_task(info["inventory"], self.task_question)
+        # limit craft and smelt goal to 150 and 200 steps before replanning
+        elif curr_goal_type == "craft" and self.goal_eps > 150:
+            self.replan_task(info["inventory"], self.task_question)
+        elif curr_goal_type == "smelt" and self.goal_eps > 200:
+            self.replan_task(info["inventory"], self.task_question)
+
+        # limit the # replanning rounds to 12
+        if self.replan_rounds > 12:
+            logger.info(f"{t}: replanning over rounds")
+            return None, "Too many replanning rounds."
+
+        # get the rolling window of the actions
+        sf = 5  # skip frame
+        wl = 10  # window len
+        end = self.actions.shape[0] - 1
+        rg = torch.arange(
+            end, min(max(end - sf * (wl - 1) - 1, self.seek_point - 1), end - 1), -sf
+        ).flip(0)
+
+        goal = list(self.curr_goal["object"].keys())[0]
+        if curr_goal_type in ["craft", "smelt"]:
+            preconditions = self.curr_goal["precondition"].keys()
+            action = self.crafter.get_action(preconditions, curr_goal_type, goal)
+        elif curr_goal_type == "mine":
+            clip_goal = self.goal_mapping_dict[goal]
+            complete_states = slice_obs(self.states, rg)
+            complete_states["prev_action"] = self.actions[rg]
+            action = self.miner.get_action(clip_goal, complete_states)
+        else:
+            logger.info("Undefined action type !!")
+
+        # check if the inventory has the preconditions
+        if len(self.curr_goal["precondition"].keys()):
+            for cond in self.curr_goal["precondition"].keys():
+                if cond not in [
+                    "wooden_pickaxe",
+                    "stone_pickaxe",
+                    "iron_pickaxe",
+                    "diamond_pickaxe",
+                    "wooden_axe",
+                    "stone_axe",
+                    "iron_axe",
+                    "diamond_axe",
+                ]:
+                    continue
+                if info["inventory"][0]["name"] != cond:
+                    for item in info["inventory"]:
+                        if (
+                            item["name"] == cond
+                            and item["quantity"] > 0
+                            and item["index"] > 0
+                        ):
+                            act = self.no_op.copy()
+                            act[5] = 5
+                            act[7] = item["index"]
+                            self.env.step(act)
+                            break
+
+        # convert the action to numpy
+        if torch.is_tensor(action):
+            action = action.cpu().numpy()
+
+        # update actions history
+        self.actions = torch.cat(
+            [self.actions, torch.from_numpy(action).unsqueeze(0)], dim=0
+        )
+
+        # update the goal_eps
+        self.goal_eps += 1
+
+        return action, curr_goal["name"]
+
+    def save_dialogue(self, filename: str):
+        self.planner.save_dialogue(filename)
+
+    def save_goal_history(self, filename: str):
+        with open(filename, "w") as f:
+            for item in self.goal_history:
+                f.write(json.dumps(item) + "\n")
+
+    def save_logs(self, output_dir: str, task_name: str, iteration: int):
+        self.save_dialogue(f"{output_dir}/{task_name}/{task_name}_{iteration}.txt")
+        self.save_goal_history(
+            f"{output_dir}/{task_name}/{task_name}_{iteration}_goal_history.jsonl"
+        )
--- main.py
+++ main.py
@@ -7,22 +7,16 @@ import hydra
 import torch
 from minedojo import MineDojoEnv
 
-from models import CraftAgent, MineAgent
-from models.utils import preprocess_obs, save_frames_to_video, slice_obs, stack_obs
-from planner import Planner
+from models.utils import preprocess_obs, save_frames_to_video
+from dep import DEP
 
 warnings.filterwarnings("ignore")
 
 logger = logging.getLogger(__name__)
 
 
-prefix = os.getcwd()
-goal_mapping_json = os.path.join(prefix, "data/goal_mapping.json")
-
 with open("data/task_info.json", "r") as f:
     TASK_INFO = json.load(f)
-
-prefix = os.getcwd()
 
 
 class Evaluator:
@@ -42,20 +36,11 @@ class Evaluator:
             force_slow_reset_interval=cfg["eval"]["num_evals"],
         )
 
-        self.goal_mapping_cfg = self.load_goal_mapping_config()
-        self.mineclip_prompt_dict = self.goal_mapping_cfg["mineclip"]
-
-        # unify the mineclip and clip
-        self.clip_prompt_dict = self.goal_mapping_cfg["clip"]
-        self.goal_mapping_dict = self.goal_mapping_cfg["horizon"]
-
         self.record_frames = cfg["record"]["frames"]
         self.frames = []
 
+        self.model = DEP(cfg, self.env)
         self.no_op = self.env.action_space.no_op()
-        self.miner = MineAgent(cfg, no_op=self.no_op)
-        self.crafter = CraftAgent(self.env, no_op=self.no_op)
-        self.planner = Planner()
 
     def reset(self, task_name: str, iteration: int = 0):
         # check that task directory exists
@@ -65,50 +50,8 @@ class Evaluator:
         self.task_name = task_name
         self.task = TASK_INFO[task_name]
         self.iteration = iteration
-        self.goal_history = []
 
-        self.planner.reset()
-        plan = self.planner.initial_planning(self.task["group"], self.task["question"])
-        self.goal_list = self.planner.generate_goal_list(plan)
-        # NOTE: if the goal list is empty, then the DEPS default goal is to mine a log
-        if len(self.goal_list) == 0:
-            self.curr_goal = {
-                "name": "mine_log",
-                "type": "mine",
-                "object": {"log": 1},
-                "precondition": {},
-                "ranking": 1,
-            }
-        else:
-            self.curr_goal = self.goal_list[0]
-        self.goal_eps = 0
-        self.replan_rounds = 0
-
-    def load_goal_mapping_config(self):
-        with open(goal_mapping_json, "r") as f:
-            goal_mapping_cfg = json.load(f)
-        return goal_mapping_cfg
-
-    # check if the inventory has the object items
-    def check_inventory(
-        self, inventory, items: dict
-    ):  # items: {"planks": 4, "stick": 2}
-        for key in items.keys():  # check every object item
-            if (
-                sum([item["quantity"] for item in inventory if item["name"] == key])
-                < items[key]
-            ):
-                return False
-        return True
-
-    def check_precondition(self, inventory, precondition: dict):
-        for key in precondition.keys():  # check every object item
-            if (
-                sum([item["quantity"] for item in inventory if item["name"] == key])
-                < precondition[key]
-            ):
-                return False
-        return True
+        self.model.reset(self.task["question"])
 
     def check_done(self, inventory, task_obj: str):
         for item in inventory:
@@ -116,165 +59,29 @@ class Evaluator:
                 return True
         return False
 
-    def update_goal(self, inventory):
-        if (
-            self.check_inventory(inventory, self.curr_goal["object"])
-            and self.goal_eps > 1
-        ):
-            logger.info(f"finish goal {self.curr_goal['name']}.")
-            self.planner.generate_success_description(self.curr_goal["ranking"])
-            self.goal_list.remove(self.goal_list[0])
-            self.curr_goal = self.goal_list[0]
-            self.goal_eps = 0
-
-    def replan_task(self, inventory, task_question):
-        self.planner.generate_failure_description(self.curr_goal["ranking"])
-        self.planner.generate_inventory_description(inventory)
-        self.planner.generate_explanation()
-        plan = self.planner.replan(task_question)
-
-        self.goal_list = self.planner.generate_goal_list(plan)
-        if len(self.goal_list) == 0:
-            self.curr_goal = {
-                "name": "mine_log",
-                "type": "mine",
-                "object": {"log": 1},
-                "precondition": {},
-                "ranking": 1,
-            }
-        else:
-            self.curr_goal = self.goal_list[0]
-        self.goal_eps = 0
-        self.replan_rounds += 1
-
     @torch.no_grad()
     def eval_step(self):
         obs = self.env.reset()
-
         logger.info(f"Evaluating the task is {self.task_name}")
-        # log start position
         logger.info(f"Start position: {obs['compass']} {obs['gps']}")
 
-        self.frames = [(obs["rgb"], "start")]
-
+        obs, _, _, info = self.env.step(self.no_op.copy())
         obs = preprocess_obs(obs)
-        states = obs
-        actions = torch.zeros(1, self.miner.model.action_dim)
-        curr_goal = None
-        prev_goal = None
-
-        seek_point = 0
-        obs, reward, A, info = self.env.step(self.no_op.copy())
-        logger.info(f"A: {A}")
-        env_done = False
+        self.frames = [(obs["rgb"], "start")]
+        success = False
 
         for t in range(0, self.task["episode"]):
-            self.update_goal(info["inventory"])
-            curr_goal = self.curr_goal
-            self.goal_history.append(curr_goal)
-
-            if t % 20 == 0:
-                logger.info(f"Episode Step {t}, Current Goal {curr_goal['name']}")
-
-            if not prev_goal == curr_goal:
-                logger.info(f"Episode Step {t}, Current Goal {curr_goal}")
-                seek_point = t
-                actions = torch.zeros(actions.shape[0], self.miner.model.action_dim)
-
-            prev_goal = curr_goal
-
-            # take the current goal type
-            curr_goal_type = self.curr_goal["type"]
-
-            # get the rolling window of the actions
-            sf = 5  # skip frame
-            wl = 10  # window len
-            end = actions.shape[0] - 1
-            rg = torch.arange(
-                end, min(max(end - sf * (wl - 1) - 1, seek_point - 1), end - 1), -sf
-            ).flip(0)
-
-            # DONE: change the craft agent into craft actions
-            goal = list(self.curr_goal["object"].keys())[0]
-            if curr_goal_type in ["craft", "smelt"]:
-                preconditions = self.curr_goal["precondition"].keys()
-                action = self.crafter.get_action(preconditions, curr_goal_type, goal)
-            elif curr_goal_type == "mine":
-                clip_goal = self.goal_mapping_dict[goal]
-                complete_states = slice_obs(states, rg)
-                complete_states["prev_action"] = actions[rg]
-                action = self.miner.get_action(clip_goal, complete_states)
-            else:
-                logger.info("Undefined action type !!")
-
-            # check if the inventory has the preconditions
-            if len(self.curr_goal["precondition"].keys()):
-                for cond in self.curr_goal["precondition"].keys():
-                    if cond not in [
-                        "wooden_pickaxe",
-                        "stone_pickaxe",
-                        "iron_pickaxe",
-                        "diamond_pickaxe",
-                        "wooden_axe",
-                        "stone_axe",
-                        "iron_axe",
-                        "diamond_axe",
-                    ]:
-                        continue
-                    if info["inventory"][0]["name"] != cond:
-                        for item in info["inventory"]:
-                            if (
-                                item["name"] == cond
-                                and item["quantity"] > 0
-                                and item["index"] > 0
-                            ):
-                                act = self.no_op.copy()
-                                act[5] = 5
-                                act[7] = item["index"]
-                                self.env.step(act)
-                                break
-            #! indent change
-            if torch.is_tensor(action):
-                action = action.cpu().numpy()
-
-            obs, _, B, info = self.env.step(action)
-            if B:
-                logger.info(f"B: {B}")
-                logger.info(
-                    f"Episode Step {t}, Current Goal {curr_goal['name']}, action {action}"
-                )
-
-            # append the video frames
-            self.frames.append((obs["rgb"], curr_goal["name"]))
-
+            action, goal_name = self.model.step(obs, info)
+            obs, _, _, info = self.env.step(action)
             obs = preprocess_obs(obs)
 
-            if not torch.is_tensor(action):
-                action = torch.from_numpy(action)
+            # append the video frames
+            self.frames.append((obs["rgb"], goal_name))
 
-            states = stack_obs(states, obs)
-            actions = torch.cat([actions, action.unsqueeze(0)], dim=0)
-
-            self.goal_eps += 1
-            if curr_goal_type == "mine" and not self.check_precondition(
-                info["inventory"], self.curr_goal["precondition"]
-            ):
-                self.replan_task(info["inventory"], self.task["question"])
-            elif curr_goal_type == "craft" and self.goal_eps > 150:
-                self.replan_task(info["inventory"], self.task["question"])
-            elif curr_goal_type == "smelt" and self.goal_eps > 200:
-                self.replan_task(info["inventory"], self.task["question"])
-
-            if self.replan_rounds > 12:
-                logger.info(f"{t}: replanning over rounds")
-                break
-
-            if self.check_done(
-                info["inventory"], self.task["object"]
-            ):  # check if the task is done?
-                env_done = True
-                logger.info(f"{t}: finish goal {self.curr_goal['name']}.")
-                self.planner.generate_success_description(self.curr_goal["ranking"])
+            # check if the task is done?
+            if self.check_done(info["inventory"], self.task["object"]):
+                success = True
+                logger.info(f"{t}: finish goal {goal_name}.")
                 break
 
         # record the video
@@ -287,19 +94,9 @@ class Evaluator:
             save_frames_to_video(self.frames, video_path)
             self.frames = []
 
-        self.planner.save_dialogue(
-            f"{self.output_dir}/{self.task_name}/{self.task_name}_{self.iteration}.txt"
-        )
+        self.model.save_logs(self.output_dir, self.task_name, self.iteration)
 
-        # save goal history as jsonl
-        with open(
-            f"{self.output_dir}/{self.task_name}/{self.task_name}_{self.iteration}_goal_history.jsonl",
-            "w",
-        ) as f:
-            for item in self.goal_history:
-                f.write(json.dumps(item) + "\n")
-
-        return env_done, t  # True or False, episode length
+        return success, t  # True or False, episode length
 
     def evaluate_task(self, task_name: str):
         num_evals = self.cfg["eval"]["num_evals"]
@@ -310,10 +107,10 @@ class Evaluator:
                 self.reset(task_name, iteration=i)
                 succ_flag, min_episode = self.eval_step()
             except Exception as e:
-                logger.info(e)
+                logger.error(e)
                 succ_flag = False
                 min_episode = 0
-                # raise e
+            # raise e
             success_rate += succ_flag
             if succ_flag:
                 episode_lengths.append(min_episode)
@@ -348,9 +145,6 @@ class Evaluator:
 def main(cfg):
     logger.info(cfg)
     output_dir = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
-
-    # save config and code to the output directory
-
     evaluator = Evaluator(cfg, output_dir=output_dir)
     evaluator.evaluate(cfg["eval"]["tasks"])
 
