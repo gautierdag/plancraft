f65ec73734708844405830a7fa88c957b3d8f106
Gautier Dagan <gautier.dagan@ed.ac.uk>
2024-04-25 16:51:00 +0000

    refactoring scripts to work with new env

--- main.py
+++ main.py
@@ -6,44 +6,37 @@ import warnings
 import hydra
 import torch
 
-# from minedojo import MineDojoEnv
-from pyvirtualdisplay import Display
-
-from plancraft.utils import preprocess_obs, save_frames_to_video
-from plancraft.oracle import OraclePlanner
+from plancraft.environments.env_real import RealPlancraft
+from plancraft.environments.env_symbolic import SymbolicPlancraft
 
 warnings.filterwarnings("ignore")
-
 logger = logging.getLogger(__name__)
 
 
-with open("data/task_info.json", "r") as f:
-    TASK_INFO = json.load(f)
-
-
 class Evaluator:
-    def __init__(self, cfg, output_dir: str, biome: str = "plains"):
+    def __init__(self, cfg, output_dir: str, mode):
         self.cfg = cfg
         self.output_dir = output_dir
+
+        # self.env = MineDojoEnv(cfg)
         # self.env =
 
         self.record_frames = cfg["record"]["frames"]
         self.frames = []
 
-        if cfg["eval"]["model"] == "oracle":
-            self.model = OraclePlanner(cfg, self.env)
-        else:
-            raise ValueError(f"Model {cfg['eval']['model']} not supported")
+        # if cfg["eval"]["model"] == "oracle":
+        #     self.model = OraclePlanner(cfg, self.env)
+        # else:
+        #     raise ValueError(f"Model {cfg['eval']['model']} not supported")
 
-        self.no_op = self.env.action_space.no_op()
+        # self.no_op = self.env.action_space.no_op()
 
     def reset(self, task_name: str, iteration: int = 0):
         # check that task directory exists
         logger.info(f"resetting the task {task_name}")
         self.task_name = task_name
-        self.task = TASK_INFO[task_name]
+        # self.task = TASK_INFO[task_name]
         self.iteration = iteration
-
         self.model.reset(self.task["question"])
 
     def check_done(self, inventory, task_obj: str):
@@ -71,11 +64,10 @@ class Evaluator:
 def main(cfg):
     logger.info(cfg)
     # output_dir = hydra.core.hydra_config.HydraConfig.get().runtime.output_dir
-
-    with Display(size=(480, 640), visible=False) as disp:
-        # display is active
-        logger.info(f"display is alive: {disp.is_alive()}")
-        # evaluate(cfg, output_dir)
+    # with Display(size=(480, 640), visible=False) as disp:
+    # display is active
+    # logger.info(f"display is alive: {disp.is_alive()}")
+    # evaluate(cfg, output_dir)
 
 
 if __name__ == "__main__":
